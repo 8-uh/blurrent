aad86cf7d50994c7d2e25f1d1a839ddc3c5cc0fb
Jeremy Dowell <jeremy@codevinsky.com>
2014-08-18 08:06:16 -0500

    Procfile

--- public/javascripts/client.js
+++
@@ -1,9 +0,0 @@
-
-sloth({
-    on: document.getElementsByTagName('img'),
-    threshold: 100,
-    callback: function(element){
-      console.log('visible:', element);
-      element.src = element.dataset.src;
-    }
-});
