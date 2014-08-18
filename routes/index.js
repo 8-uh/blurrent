50d26e1d872264e054152b1f6c4956bb022aba9f
Jeremy Dowell <jeremy@codevinsky.com>
2014-08-18 16:52:30 -0500

    Meta Info

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,8 @@ router.get('/', function(req, res) {
   ArticleService.getRandom(16, 'general')
   .then(function(articles) {
     res.render('index', {
-      title: 'Blurrent - The Best News Site On The Internet', 
+      title: 'Blurrent - The Best News Site On The Internet',
+      metaImage: '', 
       sliderArticles: [articles[0],articles[1],articles[2]], 
       blockArticles: [articles[3],articles[4],articles[5]], 
       topNews: [articles[6], articles[7], articles[8], articles[9], articles[10], articles[11]],
@@ -38,6 +39,7 @@ router.get('/nerdy', function(req, res) {
   .then(function(articles) {
     res.render('index', {
       title: 'Blurrent: Nerdy - The Best Nerdy News Site On The Internet', 
+      metaImage: '',
       sliderArticles: [articles[0],articles[1],articles[2]], 
       blockArticles: [articles[3],articles[4],articles[5]], 
       topNews: [articles[6], articles[7], articles[8], articles[9], articles[10], articles[11]],
@@ -52,7 +54,11 @@ router.get('/article/:id', function(req, res) {
       ArticleService.getRandom(6,article.tags[0])
       .then(function(articles) {
         console.log('articles:', articles);
-        res.render('article', { title: article.title, article: article, navArticles: _.first(articles,2), rightArticles: _.last(articles,4) });
+        res.render('article', { title: article.title, 
+          article: article, 
+          metaImage: article.image.link,
+          navArticles: _.first(articles,2), 
+          rightArticles: _.last(articles,4) });
       });
     });
 });
